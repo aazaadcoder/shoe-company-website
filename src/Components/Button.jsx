a2407bc73bacd8a069dee8c306f5c7ee39dbd386
aazaadcoder <128458378+aazaadcoder@users.noreply.github.com>
2025-03-21 17:36:15 +0530

    add super quality section to landing page and correct the Button component name

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-const Buttton = ({label,iconURL} ) => {
+const Button = ({label,iconURL} ) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 font-montserrat tex-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
         {label}
-        <img 
-        src={iconURL} 
-        alt={label}
-        className='ml-2 rounded-full size-5'
-        />
+        {iconURL && (
+          <img 
+          src={iconURL} 
+          alt={label}
+          className='ml-2 rounded-full size-5'
+          />
+        )}
     </button>
   )
 }
 
-export default Buttton
+export default Button
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import Button from "../Components/Button";
+import { shoe4, shoe8 } from "../assets/images";
 
 const SuperQuality = () => {
   return (
-    <div>
-      SuperQuality
-    </div>
-  )
-}
+    <section id="superquality" className="max-container flex justify-between items-center max-lg:flex-col gap-10 ">
 
-export default SuperQuality
+        <div className="flex  flex-col">
+          <h1 className="text-4xl font-bold font-palanquin capitalize lg:max-w-lg">
+            We Provide You <span className="text-coral-red">super Quality</span> Shoes</h1>
+
+          <p className="text-slate-gray lg:max-w-xl font-montserrat text-lg leading-7 mt-4">
+            Ensuring premium comfort and style, our meticulously crafted
+            footwear is designed to elevate your experience, providing you with
+            unmatched quality, innovation, and a touch of elegance.
+          </p>
+
+          <p className="text-lg leading-7 mt-6 text-slate-gray lg:max-w-lg font-montserrat">
+            Our dedication to detail and excellence ensures your satisfaction
+          </p>
+
+          <div className="mt-11">
+          <Button
+          label ="View"
+          />
+          </div>
+        </div>
+        <div>
+          <img 
+          src={shoe8} alt="" />
+        </div>
+
+    </section>
+  );
+};
+
+export default SuperQuality;
